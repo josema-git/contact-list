27c4a8c54cd65e66c88be577179ebec51ca8e27a
Jose Manuel <joseman6575@gmail.com>
2024-04-02 13:49:28 -0500

    changes

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,35 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+  return {
+    store: {
+      people: [],
+    },
+    actions: {
+      loadData: async () => {
+        const store = getStore();
+        const response = await fetch(
+          "https://playground.4geeks.com/contact/docs"
+        );
+        const data = await response.json();
+        setStore({ people: data });
+      },
+      addPerson: (person) => {
+        const store = getStore();
+        setStore({ people: [...store.people, person] });
+      },
+      deletePerson: (index) => {
+        const store = getStore();
+        const newPeople = store.people.filter((person, i) => i !== index);
+        setStore({ people: newPeople });
+      },
+      editPerson: (index, person) => {
+        const store = getStore();
+        const newPeople = store.people.map((p, i) =>
+          i === index ? person : p
+        );
+        setStore({ people: newPeople });
+      },
+    },
+  };
 };
 
 export default getState;
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,38 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
-import "../../styles/home.css";
+import React, { useState, useEffect, useContext } from "react";
+import { Context } from "../store/appContext";
+import { Button, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+  const [people, setPeople] = useState([]);
+  const { store, actions } = useContext(Context);
+
+  useEffect(() => {
+    actions.loadData();
+  });
+
+  return (
+    <Container className="pt-3">
+      <div className="d-flex justify-content-end">
+        <Button></Button>
+      </div>
+      <div className="d-flex justify-content-between">
+        {store &&
+          store.map((person, index) => <Contact item={person} key={index} />)}
+      </div>
+    </Container>
+  );
+};
+
+const Contact = ({ item }) => {
+  return (
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">{item.full_name}</h5>
+        <h6 className="card-subtitle mb-2 text-muted">{item.email}</h6>
+        <p className="card-text">{item.address}</p>
+        <p className="card-text">{item.phone}</p>
+      </div>
+    </div>
+  );
+};
